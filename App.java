a9595739496f6fa790d926eea169d3c401cfa046
Masto Sitorus <if08017@users.noreply.github.com>
2016-01-31 01:50:06 +0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+package ec2-52-77-232-42.ap-southeast-1.compute.amazonaws.com;
+ 
+public class App{
+    
+    public static void main( String[] args ){
+        App app=new App();
+        String input="Selamat Belajar Maven";
+        System.out.println("Panjang dari "+input+" adalah "+app.stringLength(input));
+    }
+    
+    public int stringLength(String input){
+        return input.length();
+    }
+}
