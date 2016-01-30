cf4b3e6c5c6b669bef883dd231a543d47f442c8d
Masto Sitorus <if08017@users.noreply.github.com>
2016-01-31 01:59:14 +0700

    Delete TestBankAccount.java

--- TestBankAccount.java
+++
@@ -1,9 +0,0 @@
-package c2-52-77-232-42.ap-southeast-1.compute.amazonaws.com;
-
-import junit.framework.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-public class TestBankAccount {
-
-}
